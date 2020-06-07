d5edbb52719cc811a2ffcfac5ff6009f1fb32864
Rayhane.Hamoumi <38473988+rayhaneHamoumi@users.noreply.github.com>
2020-06-07 13:22:34 +0200

    2 sensors toegevoegd : temperatuur en detecor
    arduino code

--- Arduino_IOT_FinalWork/Arduino_IOT_FinalWork.ino
+++ Arduino_IOT_FinalWork/Arduino_IOT_FinalWork.ino
@@ -1,35 +1,83 @@
+#define led 7
+#define buzzer 8
+#define pirSensor 9
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+
+static const int DHT_SENSOR_PIN = 2;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+
+
 /*
-* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
-*
-* by Dejan Nedelkovski,
-* www.HowToMechatronics.com
-*
+   Initialize the serial port.
 */
-// defines pins numbers
-const int trigPin = 9;
-const int echoPin = 10;
-boolean SendToSerial;
-// defines variables
-long duration;
-int distance;
-void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-Serial.begin(9600); // Starts the serial communication
+void setup( )
+{
+  Serial.begin(9600);
+  pinMode(pirSensor, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 }
-void loop() {
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
+
+
+
+/*
+   Poll for a measurement, keeping the state machine alive.  Returns
+   true if a measurement is available.
+*/
+static bool measure_environment( float *temperature, float *humidity )
+{
+  {
+    if ( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      return ( true );
+    }
+  }
+
+  return ( false );
+}
+
+
+
+/*
+   Main program loop.
+*/
+void loop( )
+{
+  Temperatuur();
+  Capteur();
+}
+
+void Temperatuur() {
+  float temperature;
+  float humidity;
+
+  /* Measure temperature and humidity.  If the functions returns
+     true, then a measurement is available. */
+  if ( measure_environment( &temperature, &humidity ) == true )
+  {
+    Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+  }
+}
+
+void Capteur()
+{
+  int x = digitalRead(pirSensor);
+  if (x == LOW)
+  {
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    Serial.println(x);
+  }
+  else
+  {
+    digitalWrite(led, HIGH);
+    digitalWrite(buzzer, HIGH);
+    Serial.println(x);
+  }
 }
