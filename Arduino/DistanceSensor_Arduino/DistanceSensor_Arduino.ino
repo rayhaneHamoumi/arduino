b574b8b59384aaf8e5c9bf3c5b3a913382161027
Rayhane.Hamoumi <38473988+rayhaneHamoumi@users.noreply.github.com>
2020-06-08 19:35:45 +0200

    Arduino files voor de sensors aangemaakt + verijderen van onnodige files

---
+++ Arduino/DistanceSensor_Arduino/DistanceSensor_Arduino.ino
@@ -0,0 +1,18 @@
+//www.elegoo.com
+//2016.12.08
+#include "SR04.h"
+#define TRIG_PIN 10
+#define ECHO_PIN 11
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long a;
+
+void setup() {
+   Serial.begin(9600);
+   delay(5000);
+}
+
+void loop() {
+   a=sr04.Distance();
+   Serial.println(a);
+   delay(5000);
+}
