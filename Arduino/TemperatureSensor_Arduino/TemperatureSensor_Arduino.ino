8aab6a9ecec4c1c4fd27494e3d7d6eaa0dce64f0
Rayhane.Hamoumi <38473988+rayhaneHamoumi@users.noreply.github.com>
2020-06-08 23:29:50 +0200

    Temperature Sensor met lcd scherm

--- Arduino/TemperatureSensor_Arduino/TemperatureSensor_Arduino.ino
+++ Arduino/TemperatureSensor_Arduino/TemperatureSensor_Arduino.ino
@@ -1,54 +1,71 @@
-/*-----------Arduino LM35 Code-------------*/
+#include <dht.h>
+#include <LiquidCrystal.h>
+#define LCD_LIGHT_PIN A4
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-/*-----------Digital Thermometer Using Arduino-------------*/
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
 
-#include<LiquidCrystal.h>
-LiquidCrystal lcd(7,6,5,4,3,2);
+static const int DHT_SENSOR_PIN = 7;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
-#define sensor A0
 
-byte degree[8] = 
-              {
-                0b00011,
-                0b00011,
-                0b00000,
-                0b00000,
-                0b00000,
-                0b00000,
-                0b00000,
-                0b00000
-              };
 
-void setup()
+/*
+   Initialize the serial port.
+*/
+void setup( )
 {
-  Serial.begin(9600);
-  lcd.begin(16,2);
-  lcd.createChar(1, degree);
-  lcd.setCursor(0,0);
-  lcd.print("    Digital    ");
-  lcd.setCursor(0,1);
-  lcd.print("  Thermometer   ");
-  delay(4000);
-  lcd.clear();
-  lcd.print(" Circuit Digest  ");
-  delay(4000);
-  lcd.clear();
+  Serial.begin( 9600);
+  lcd.begin(16, 2);
+  pinMode(LCD_LIGHT_PIN, OUTPUT);
+  digitalWrite(LCD_LIGHT_PIN, HIGH);
 }
 
-void loop()
+
+
+/*
+   Poll for a measurement, keeping the state machine alive.  Returns
+   true if a measurement is available.
+*/
+static bool measure_environment( float *temperature, float *humidity )
 {
-  /*---------Temperature-------*/
-     float reading=analogRead(sensor);
-     float temperature=reading*(5.0/1023.0)*100;
-     delay(10);
-  
-  /*------Display Result------*/
-    lcd.clear();
-    lcd.setCursor(2,0);
-    lcd.print("Temperature");
-    lcd.setCursor(4,1);
-    lcd.print(temperature);
-    lcd.write(1);
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if ( millis( ) - measurement_timestamp > 3000ul )
+  {
+    if ( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      measurement_timestamp = millis( );
+      return ( true );
+    }
+  }
+
+  return ( false );
+}
+
+
+
+/*
+   Main program loop.
+*/
+void loop( )
+{
+  float temperature;
+  float humidity;
+
+  /* Measure temperature and humidity.  If the functions returns
+     true, then a measurement is available. */
+  if ( measure_environment( &temperature, &humidity ) == true )
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(temperature, 1);
+    lcd.print((char)223);
     lcd.print("C");
-    delay(1000);
+    lcd.setCursor(0, 1);
+    lcd.print(" IOT  Automatic");
+    Serial.println( temperature, 1 );
+  }
 }
